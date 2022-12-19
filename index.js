60df2db8b0682cfda5aed0945a48521b1775b691
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2022-12-19 15:45:23 +0300

    get request to API

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const name = document.getElementById("inputBreed");
 const dogName = document.getElementById("dogTitle");
 const image = document.getElementById("dogImg");
 const likes = document.getElementById("likesCount");
+var dog  = 'golden retriever'
+
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/dogs?name=' + name,
+    headers: { 'X-Api-Key': 'live_ns0Z6Hxx6sjhdboz3RoUYTYue9HkkpcVTnx0CYMsYaGaWZVJk2aQzIhk7qj7ReCX'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
 
 //adding like event
 function likesCount(){
