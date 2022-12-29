0603e0bec96692576dca66ac96944eccec28c997
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2022-12-29 10:30:04 +0300

    api config

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 
-const name = document.getElementById("inputBreed");
-const dogName = document.getElementById("dogTitle");
-const image = document.getElementById("dogImg");
+var name = document.getElementById("selectBreed");
+var dogName = document.getElementById("dogTitle");
+var image = document.getElementById("dogImg");
 const likes = document.getElementById("likesCount");
-var dog  = 'golden retriever'
+var dog  = 'german shepherd'
 
-$.ajax({
+function fetchApiNinja() {
+    fetch('https://api.api-ninjas.com/v1/dogs?name=' + dog,{
+    method: "GET",
+    headers: {'X-Api-Key': 'txnBMmne4+CBpk7YCQcS5g==JCz1gtAHw8pVigCh'},
+})
+    .then(response => response.json()) 
+    .then(data => {
+        image.src = data.image_link;
+        dogName.innerHTML = data.name;
+        console.log('energy');
+    }) 
+    .catch(err => console.log(err));
+};
+
+
+/*S$.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/dogs?name=' + dog,
-    headers: { 'X-Api-Key': 'live_ns0Z6Hxx6sjhdboz3RoUYTYue9HkkpcVTnx0CYMsYaGaWZVJk2aQzIhk7qj7ReCX'},
+    headers: { 'X-Api-Key': 'txnBMmne4+CBpk7YCQcS5g==JCz1gtAHw8pVigCh'},
     contentType: 'application/json',
     success: function(result) {
+        image = data.image_link;
+        dogName = data.name;
+        console.log(data.energy);
         console.log(result);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
-});
+});*/
 
 //adding like event
 function likesCount(){
     document.getElementById("likeBtn").addEventListener('click', (e)=>{
         const likes = document.getElementById("likesCount");
-        console.log('woof') 
+        console.log('woof'); 
 
     });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchApiNinja();
+});
