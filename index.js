48ec508b1d84c815c97138aebc8e6e7673ba3490
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-01-05 13:58:52 +0300

    added dog info

--- index.js
+++ index.js
@@ -2,39 +2,37 @@
 var name = document.getElementById("selectBreed");
 var dogName = document.getElementById("dogTitle");
 var image = document.getElementById("dogImg");
-const likes = document.getElementById("likesCount");
-var dog  = 'german shepherd'
+var life = document.getElementById("lifeExpectancy");
+var weightMale = document.getElementById("weightMale");
+var weightFemale = document.getElementById("weightFemale");
+var coatLength = document.getElementById("coatLength");
+var shedding = document.getElementById("shedding");
+var trainability = document.getElementById("trainability");
+var likes = document.getElementById("likesCount");
+var dog = 'labrador'
 
 function fetchApiNinja() {
     fetch('https://api.api-ninjas.com/v1/dogs?name=' + dog,{
     method: "GET",
     headers: {'X-Api-Key': 'txnBMmne4+CBpk7YCQcS5g==JCz1gtAHw8pVigCh'},
+    contentType: 'application/json',
 })
     .then(response => response.json()) 
-    .then(data => {
-        image.src = data.image_link;
-        dogName.innerHTML = data.name;
-        console.log('energy');
+    .then((dogInfo) => {
+        console.log(dogInfo);
+        image.src = dogInfo[0].image_link;
+        dogName.innerHTML = dogInfo[0].name;
+        life.innerHTML = dogInfo[0].max_life_expectancy;
+        weightMale.innerHTML = dogInfo[0].max_weight_male;
+        weightFemale.innerHTML = dogInfo[0].max_weight_female;
+        coatLength.innerHTML = dogInfo[0].coat_length;
+        shedding.innerHTML = dogInfo[0].shedding;
+        trainability.innerHTML = dogInfo[0].trainability;
+        
     }) 
     .catch(err => console.log(err));
 };
 
-
-/*S$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/dogs?name=' + dog,
-    headers: { 'X-Api-Key': 'txnBMmne4+CBpk7YCQcS5g==JCz1gtAHw8pVigCh'},
-    contentType: 'application/json',
-    success: function(result) {
-        image = data.image_link;
-        dogName = data.name;
-        console.log(data.energy);
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});*/
 
 //adding like event
 function likesCount(){
